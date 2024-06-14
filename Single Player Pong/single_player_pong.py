7e17bea3bc2309afc0e9d14f7ea5b45bbc957dec
Mariana Baroni <mabaronif@gmail.com>
2024-06-14 21:36:34 +0900

    Fixed the paddle movement, broke the ball movement (up/down)

--- Single Player Pong/single_player_pong.py
+++ Single Player Pong/single_player_pong.py
@@ -2,8 +2,8 @@ from tkinter import Canvas
 import time
 
 # Canvas's size
-CANVAS_WIDTH = 350
-CANVAS_HEIGHT = 350
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
 
 BALL_SIZE = 20
 
@@ -19,17 +19,17 @@ def main():
     # Sets the canvas edges
     canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    # Canvas's dimensions
-    middle_x = CANVAS_WIDTH/2
-    middle_y = CANVAS_HEIGHT/2
+    # Canvas's center
+    center_x = CANVAS_WIDTH/2
+    center_y = CANVAS_HEIGHT/2
 
     # Paddle's start coordinates
-    paddle_left_x = middle_x - PADDLE_WIDTH / 2
+    paddle_left_x = center_x - PADDLE_WIDTH / 2
     paddle_start_y = CANVAS_HEIGHT - PADDLE_HEIGHT
 
-    # Start speed
-    x_speed = INITIAL_PADDLE_SPEED
-    y_speed = 0
+    # Paddle's start speed
+    paddle_x_speed = INITIAL_PADDLE_SPEED
+    paddle_y_speed = 0
 
     # Ball
     ball = canvas.create_oval(0, 0, BALL_SIZE, BALL_SIZE, 'blue')
@@ -44,25 +44,26 @@ def main():
         CANVAS_HEIGHT,
         "white", "navy")
 
-    #Defines the paddle's movement
     while True:
-        current_location = canvas.move(paddle, x_speed, y_speed)
+        # Defines the paddle's keyboard movement
+        current_location = canvas.move(paddle, paddle_x_speed, paddle_y_speed)
 
         # Makes the paddle move with key press command 
         key = canvas.get_last_key_press()
-        if key == 'ArrowLeft':
-            x_speed = -INITIAL_PADDLE_SPEED
-            y_speed = 0
-        if key == 'ArrowRight':
-            x_speed = INITIAL_PADDLE_SPEED
-            y_speed = 0
+        if key == 'ArrowLeft' and paddle_left_x > 0:
+            paddle_x_speed = -INITIAL_PADDLE_SPEED
+            paddle_y_speed = 0
+        if key == 'ArrowRight' and paddle_right_x < CANVAS_WIDTH:
+            paddle_x_speed = INITIAL_PADDLE_SPEED
+            paddle_y_speed = 0
         if key == 'ArrowUp':
-            y_speed = 0
-            x_speed = 0
+            paddle_y_speed = 0
+            paddle_x_speed = 0
         if key == 'ArrowDown':
-            y_speed = 0
-            x_speed = 0
-            
+            paddle_y_speed = 0
+            paddle_x_speed = 0
+
+        # Delay rate to make it possible to see the paddle in movement.    
         time.sleep(DELAY)
         
         # Ball's current coordinates
@@ -70,37 +71,35 @@ def main():
         ball_top_y = canvas.get_top_y(ball)
         ball_bottom_y = ball_top_y + BALL_SIZE
 
-        # Paddle's current location
+            # Paddle's current location
         paddle_left_x = canvas.get_left_x(paddle)
         paddle_top_y = canvas.get_top_y(paddle)
         paddle_right_x = paddle_left_x + PADDLE_WIDTH
 
-        # It should reverse direction on x=0, but it's skipping
-        # x=1 direct to x=-1.
-        if paddle_left_x < 0:
-            paddle_left_x += INITIAL_PADDLE_SPEED
-        elif paddle_left_x >= CANVAS_WIDTH:
-            paddle_left_x -= INITIAL_PADDLE_SPEED
+        # Stops the paddle when it hits the side walls.
+        if paddle_left_x <= 0 and paddle_x_speed <0:
+            paddle_x_speed = 0
+        elif paddle_right_x >= CANVAS_WIDTH and paddle_x_speed > 0:
+            paddle_x_speed = 0
 
-        print("x:", paddle_left_x) 
-        
         # Checks if the ball hits the side walls
-        if ball_left_x < 0 or ball_bottom_y >= CANVAS_WIDTH:
+        if ball_left_x < 0 or ball_left_x + BALL_SIZE >= CANVAS_WIDTH:
             change_ball_x = -change_ball_x
         
-        # Checks if the ball hits the up and bottom walls.
-        if ball_top_y < 0 or ball_bottom_y >= CANVAS_HEIGHT:
+        # Checks if the ball hits the top wall.
+        if ball_top_y <= 0:
             change_ball_y = -change_ball_y
+        
+        # Checks if the ball hits the bottom line, then ends the game.
+        if ball_bottom_y >= CANVAS_HEIGHT:
+            print("Game over.") #Just to check if the game stops. I'll remove it later.
+            #break - to exit the while loop and end the game
+            canvas.create_text(center_x - center_x/2, center_y, font_size = 50, text="GAME OVER", color="red")           
 
         # Checks if the ball hits the paddle and bounces.   
-        # But now, after that, it doesn't bounces in the side walls.
-        if (paddle_top_y <= ball_bottom_y) and (paddle_left_x <= ball_left_x):
+        if (paddle_top_y <= ball_bottom_y) and (paddle_left_x <= ball_left_x <= paddle_right_x):
             change_ball_y = -change_ball_y   
         
-        
-        
-        # If the ball reaches the bottom wall, the game is over.
-
         canvas.move(ball, change_ball_x, change_ball_y)
         time.sleep(DELAY)
        
@@ -110,13 +109,20 @@ if __name__ == '__main__':
     main()
 
 # Milestones:
-# 1 Set up the game canvas - OK
-# 2 Create the ball and paddle - OK
-# 3 Implement paddle movement - Keybord paddle moving working, but the paddle is going off the canvas, not stopping in the side walls.
-# 4 Animate the ball - OK
-# 5 Handle ball-paddle collision - OK - but after hitting the paddle, the ball doesn't bounce off the side walls anymore. Soving this may solve the paddle border issue too.
+# 1 Set up the game canvas - DONE
+# 2 Create the ball and paddle - DONE
+# 3 Implement paddle movement - DONE
+# 4 Animate the ball - DONE
+# 5 Handle ball-paddle collision - DONE
 # 6 Implement game over
+    #6.1 Condition to end the game when the ball hits the bottom of the screen - DONE
+    #6.2.1 Display a 'Game Over' message centered on the canvas.
+    #6.2.2 Display a 'Game Over' message only after the 3rd chance.
 # 7 Add rounds and display rounds left
+    #7.1 Track the number of rounds left - 3 chances.
+    #7.2 Display the count on the bottom-left corner of the screen.
 # 8 Prompt to start the game
+    #8.1 Prompt 'Round 1" at the beginning of the game.
+    #8.2 Display 'Round 2' and 'Round 3' in the beginning of the 2 and 3 chances.
 # 9 Test and debug
 # Bonus (if there's time): add background color
